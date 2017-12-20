f8bab447fb7e0357a036f160b63615afe81a9148
simone <simone.zippo@gmail.com>
2017-12-20 10:52:33 +0100

    public ip al posto di private

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ var config = {
     //172.17.0.2
     //18.221.213.162
     // balancerIp: '34.195.19.72', // Ip statico del load balancer
-    balancerIp: '172.17.0.2', // Ip statico del load balancer
+    balancerIp: '34.195.19.72', // Ip statico del load balancer
 
     balancerPort: 6602, // Porta di ascolto del load balancer
     balancerSubPath: '/api/lb/edge/subscribe'
